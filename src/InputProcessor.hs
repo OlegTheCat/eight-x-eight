8ae3ccb5b45970770b252fa90fe5ec417fd89ab6
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2015-10-02 22:32:37 +0300

    Add handling of enter command

--- src/InputProcessor.hs
+++ src/InputProcessor.hs
@@ -5,7 +5,8 @@ import Input
 import Prelude hiding (Left, Right)
 
 processCommand :: InputCommand -> Game -> Game
-processCommand CursorUp g    = moveCursorInGame g Up
-processCommand CursorDown g  = moveCursorInGame g Down
-processCommand CursorLeft g  = moveCursorInGame g Left
+processCommand CursorUp    g = moveCursorInGame g Up
+processCommand CursorDown  g = moveCursorInGame g Down
+processCommand CursorLeft  g = moveCursorInGame g Left
 processCommand CursorRight g = moveCursorInGame g Right
+processCommand Enter       g = takeCellValue g
