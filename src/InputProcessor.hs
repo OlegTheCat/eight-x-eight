0c2282cc15962ea621131f468b226e9b78feb483
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2015-10-15 21:40:34 +0300

    More currying

--- src/InputProcessor.hs
+++ src/InputProcessor.hs
@@ -5,8 +5,8 @@ import Input
 import Prelude hiding (Left, Right)
 
 processCommand :: InputCommand -> Game -> Game
-processCommand CursorUp    g = moveCursorInGame g Up
-processCommand CursorDown  g = moveCursorInGame g Down
-processCommand CursorLeft  g = moveCursorInGame g Left
-processCommand CursorRight g = moveCursorInGame g Right
-processCommand Enter       g = takeCellValue g
+processCommand CursorUp    = moveCursorInGame Up
+processCommand CursorDown  = moveCursorInGame Down
+processCommand CursorLeft  = moveCursorInGame Left
+processCommand CursorRight = moveCursorInGame Right
+processCommand Enter       = takeCellValue
--- src/Main.hs
+++ src/Main.hs
@@ -24,8 +24,3 @@ main =
     hSetEcho stdout False
     g <- createGame
     gameLoop g
-    -- renderGame pureGame
-    -- putStrLn "========================="
-    -- print $ show pureGame
-    -- g <- createGame
-    -- renderGame g
